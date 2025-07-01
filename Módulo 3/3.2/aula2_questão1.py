8c764217b59f895af6f52baa9136d0c665f8a3ec
Marco Antônio Ferreira <ferreiramarcoantonio904@gmail.com>
2025-07-01 20:13:51 -0300

    Add files via upload

---
+++ Módulo 3/3.2/aula2_questão1.py
@@ -0,0 +1,4 @@
+C_idade = int(input("Digite é a idade do Cris: "))
+J_idade = int(input("Digite é a idade de Juliana: "))
+
+print("Permissão para entrar no bar:" + str((C_idade >= 17) and (J_idade >= 17)))
